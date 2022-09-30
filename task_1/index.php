59f7e1a95e422826c1d24ef971bbd5d11c7f342a
Elena7110 <alen.spirit@gmail.com>
2022-09-30 21:15:28 +0300

    add comments and redaction of code

--- task_1/index.php
+++ task_1/index.php
@@ -1,249 +1,198 @@
 <!DOCTYPE html>
+<!--атрибут lang определяет язык содержимого элемента.-->
 <html lang="en">
 
+<!--метаданные-->
+
 <head>
+	<!--данная запись указывает браузеру кодировку в которой была написана данная страница - формат документа и раскладку клавиатуры-->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!--призывает Internet Explorer работать в определённом режиме документа-->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!--мeta-тег viewport сообщает браузеру о том, как именно обрабатывать размеры страницы, и изменять её масштаб-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<!--определяет заголовок документа, который отображается в заголовке окна браузера или на вкладке страницы.-->
+	<title>Таблица истинности PHP</title>
+	<!--подключение файла стилей-->
 	<link rel="stylesheet" type="text/css" href="style.css" media="all">
 </head>
 
+<!--тело документа-->
+
 <body>
-	<br>
+	<!--Заголовок таблицы-->
+	<h3>Таблица истинности PHP</h3>
+	<!--Таблица-->
 	<table class="table" border="1">
+		<!--Строка-->
 		<tr>
+			<!--ячейка строки-->
 			<th>A</th>
+			<!--ячейка строки-->
 			<th>B</th>
+			<!--ячейка строки-->
 			<th>!A</th>
+			<!--ячейка строки-->
 			<th>A || B</th>
+			<!--ячейка строки-->
 			<th>A && B</th>
+			<!--ячейка строки-->
 			<th>A xor B</th>
 		</tr>
 
+		<!--Строка-->
 		<tr>
+			<!--ячейка строки-->
 			<td><?php echo $A = 0; ?></td>
+			<!--ячейка строки-->
 			<td><?php echo $B = 0; ?></td>
-
+			<!--ячейка строки-->
 			<td><?php
-if (!$A !== FALSE) {
-?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+if (!$A){
+	echo !$A; 
+			} elseif ((!$A) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A||$B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A||$B; 
+			} elseif (($A||$B) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A&&$B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A&&$B; 
+				} elseif (($A&&$B) ==''){$C=0; echo $C;
+				
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A xor $B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A xor $B; 
+				} elseif (($A xor $B) ==''){$C=0; echo $C;
+				
 }
-?></td>
+?> </td>
 
 		</tr>
 
-
+		<!--Строка-->
 		<tr>
+			<!--ячейка строки-->
 			<td><?php echo $A = 0; ?></td>
+			<!--ячейка строки-->
 			<td><?php echo $B = 1; ?></td>
-
+			<!--ячейка строки-->
 			<td><?php
-if (!$A !== FALSE) {
-?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+if (!$A){
+	echo !$A; 
+			} elseif ((!$A) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A||$B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A||$B; 
+			} elseif (($A||$B) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A&&$B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A&&$B; 
+				} elseif (($A&&$B) ==''){$C=0; echo $C;
+				
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A xor $B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A xor $B; 
+				} elseif (($A xor $B) ==''){$C=0; echo $C;
+				
 }
-?></td>
+?> </td>
+
 
 		</tr>
 
-
+		<!--Строка-->
 		<tr>
+			<!--ячейка строки-->
 			<td><?php echo $A = 1; ?></td>
+			<!--ячейка строки-->
 			<td><?php echo $B = 0; ?></td>
+			<!--ячейка строки-->
 			<td><?php
-if (!$A !== FALSE) {
-?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+if (!$A){
+	echo !$A; 
+			} elseif ((!$A) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A||$B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A||$B; 
+			} elseif (($A||$B) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A&&$B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A&&$B; 
+				} elseif (($A&&$B) ==''){$C=0; echo $C;
+				
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A xor $B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A xor $B; 
+				} elseif (($A xor $B) ==''){$C=0; echo $C;
+				
 }
-?></td>
+?> </td>
 
 		</tr>
 
-
+		<!--Строка-->
 		<tr>
+			<!--ячейка строки-->
 			<td><?php echo $A = 1; ?></td>
+			<!--ячейка строки-->
 			<td><?php echo $B = 1; ?></td>
-
+			<!--ячейка строки-->
 			<td><?php
-if (!$A !== FALSE) {
-?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+if (!$A){
+	echo !$A; 
+			} elseif ((!$A) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A||$B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A||$B; 
+			} elseif (($A||$B) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A&&$B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A&&$B; 
+				} elseif (($A&&$B) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
+			<!--ячейка строки-->
 			<td><?php
 if ($A xor $B){
-	?>
-				true
-				<?php
-} else {
-?>
-				false
-				<?php
+	echo $A xor $B; 
+				} elseif (($A xor $B) ==''){$C=0; echo $C;
 }
-?></td>
-
+?> </td>
 		</tr>
-
-
-	</table>
-
-	<br>
-
 	</table>
 </body>
 
